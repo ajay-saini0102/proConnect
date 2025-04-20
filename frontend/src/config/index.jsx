55b7afa5a93682aca13349de6f2e581a74d51bc8
Ajay Saini <153486378+ajaysaini3078@users.noreply.github.com>
2025-04-20 18:25:52 +0530

    frontend Deploy

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3030"
+export const BASE_URL = "https://proconnectlinkedinclone-e1ha.onrender.com"
 
 export const clintServer = axios.create({
   baseURL : BASE_URL,
